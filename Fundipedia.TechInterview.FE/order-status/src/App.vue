d44ff57b8072f8011bcd7953ddbfc5208ba62449
Tiago Vallejo <tiago@tvallejo.net>
2025-01-31 16:17:25 +0000

    Add vue front end skeleton

---
+++ Fundipedia.TechInterview.FE/order-status/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <OrderStatus/>
+</template>
+
+<script>
+import OrderStatus from './components/OrderStatus.vue'
+
+export default {
+  name: 'App',
+  components: {
+    OrderStatus
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
